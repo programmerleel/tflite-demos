0e2b6f9aef1220134edf97b5180593ed39fe2f91
“lishihao” <programmerleel@163.com>
2024-03-06 18:23:20 +0800

    yolov5安全帽检测模型训练

---
+++ yolov5/test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/02/27 08:50
+# @Author  : LiShiHao
+# @FileName: test.py
+# @Software: PyCharm
+
+import torch
+from torch import nn
+
+torch.distributed.init_process_group
